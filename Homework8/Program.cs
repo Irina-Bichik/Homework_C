3e3cf876a397232ad78851e6d51f604dd64cce8a
Irina <fransik@bk.ru>
2023-03-23 00:48:58 +0300

    Task 54 - SortToLower; Task 56 - RowMinSumElem; Task 60 - 3D Array

---
+++ Homework8/Program.cs
@@ -0,0 +1,178 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int row = new Random().Next(4,6);
+int column = new Random().Next(4,6);
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortToLower(matrix);
+PrintMatrix(matrix);
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int row = new Random().Next(4,6);
+int column = new Random().Next(4,6);
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RowMinSumElements(int[,] arr)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        minRow += arr[0, i];
+    }
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) sumRow += arr[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"The number of the row with the smallest sum of elements is {minSumRow + 1}");
+}
+
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+RowMinSumElements(matrix);
+
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Write("Input the number of tables: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number of rows: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number of columns: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int[,,] FillArray3D(int x, int y, int z) 
+{
+    int[,,] array = new int[x, y, z];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {      
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10,100);    
+            }            
+        }
+    }
+    return array;
+}
+
+void PrintArray3D(int[,,] array)
+{
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) \t");   
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
+
+Console.WriteLine($"Array size {x} x {y} x {z}"); 
+int [,,] matrix = FillArray3D(x,y,z);
+PrintArray3D(matrix);
